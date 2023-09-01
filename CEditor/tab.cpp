c00def0f09432fa6ff819266b210a1c5ddbf74e8
XiaoZheng2003 <1345486584@qq.com>
2023-09-01 16:25:58 +0800

    fix #64847104 文件操作功能

--- CEditor/tab.cpp
+++ CEditor/tab.cpp
@@ -1,14 +1,45 @@
 #include "tab.h"
 #include "ui_tab.h"
 
-Tab::Tab(QWidget *parent) :
+Tab::Tab(int index, QString text, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::Tab)
+    ui(new Ui::Tab),
+    curIndexId(index)
 {
     ui->setupUi(this);
+
+    ui->plainTextEdit->setPlainText(text);
 }
 
 Tab::~Tab()
 {
     delete ui;
 }
+
+void Tab::prepareTextForSave(int indexId)
+{
+    //返回文件保存文本
+    if(indexId!=curIndexId) return;
+    emit returnTextForSave(indexId,ui->plainTextEdit->toPlainText());
+}
+
+void Tab::prepareTextForSaveAs(int indexId)
+{
+    //返回文件另存为文本
+    if(indexId!=curIndexId) return;
+    emit returnTextForSaveAs(indexId,ui->plainTextEdit->toPlainText());
+}
+
+void Tab::tabClosed(int indexId)
+{
+    //关闭标签
+    if(indexId==curIndexId) curIndexId=-1; //当前标签被关闭
+    if(indexId<curIndexId) curIndexId--;   //当前标签前面的标签被关闭
+}
+
+void Tab::on_plainTextEdit_textChanged()
+{
+    //当文本被修改
+    emit textChanged(curIndexId);
+}
+
--- CEditor/tab.h
+++ CEditor/tab.h
@@ -12,11 +12,25 @@ class Tab : public QDialog
     Q_OBJECT
 
 public:
-    explicit Tab(QWidget *parent = nullptr);
+    explicit Tab(int index, QString text, QWidget *parent = nullptr);
     ~Tab();
+
+public slots:
+    void prepareTextForSave(int indexId);
+    void prepareTextForSaveAs(int indexId);
+    void tabClosed(int indexId);
+
+signals:
+    void returnTextForSave(int indexId, QString str);
+    void returnTextForSaveAs(int indexId, QString str);
+    void textChanged(int indexId);
+
+private slots:
+    void on_plainTextEdit_textChanged();
 
 private:
     Ui::Tab *ui;
+    int curIndexId;
 };
 
 #endif // TAB_H
