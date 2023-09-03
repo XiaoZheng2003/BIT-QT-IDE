42490003e2961c01d779b7a9d14b41b9ca72a23a
A1120212726 <277645218@qq.com>
2023-09-03 23:03:59 +0800

    括号匹配

--- CEditor/codeeditor.h
+++ CEditor/codeeditor.h
@@ -6,12 +6,27 @@
 #include <QWidget>
 #include <QListWidget>
 #include <QScrollBar>
+#include <QTextBlock>
+
+class Brackets // 括号匹配类
+{
+public:
+     Brackets(int currentPosition, int correspondingPosition, int bracketType) {
+         currentPos = currentPosition;
+         correspondingPos = correspondingPosition;
+         type = bracketType;
+     }
+     int currentPos = 0;
+     int correspondingPos = 0;
+     int type = 0; // 1: '{' / -1: '}'  2: '(' / -2: ')'
+};
 
 class CodeEditor : public QPlainTextEdit
 {
     Q_OBJECT
 public:
     explicit CodeEditor(QWidget *parent=nullptr);
+    void matchBrackets();
 
 protected:
     //void keyPressEvent(QKeyEvent *event);
@@ -21,6 +36,11 @@ signals:
     void updateLineNumberArea(int blockCount);
 
 private:
+    QList<Brackets> bralist; // 括号匹配情况
+
 };
+
+
+
 
 #endif // CODEEDITOR_H
